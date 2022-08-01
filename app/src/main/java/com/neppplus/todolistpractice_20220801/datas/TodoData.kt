10e61273f5394ac1692d0a13468cc00c3f2f4f78
넵플러스 <nepp.plus@gmail.com>
2022-08-01 15:14:33 +0900

    리스트뷰에서 한 칸 눌렀을 경우 Detail로 이동하는 로직 작성

--- app/src/main/java/com/neppplus/todolistpractice_20220801/datas/TodoData.kt
+++ app/src/main/java/com/neppplus/todolistpractice_20220801/datas/TodoData.kt
@@ -1,7 +1,10 @@
 package com.neppplus.todolistpractice_20220801.datas
 
+import java.io.Serializable
+
 class TodoData (
     val title : String,
     val rating : Double,
-        ) {
+    val place : String
+        ) : Serializable {
 }
--- app/src/main/java/com/neppplus/todolistpractice_20220801/fragments/TodoListFragment.kt
+++ app/src/main/java/com/neppplus/todolistpractice_20220801/fragments/TodoListFragment.kt
@@ -1,11 +1,13 @@
 package com.neppplus.todolistpractice_20220801.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.neppplus.todolistpractice_20220801.DetailTodoActivity
 import com.neppplus.todolistpractice_20220801.R
 import com.neppplus.todolistpractice_20220801.adapters.TodoListViewAdapter
 import com.neppplus.todolistpractice_20220801.datas.TodoData
@@ -31,18 +33,20 @@ class TodoListFragment : Fragment(){
         mList.clear()
 
 //        더미 데이터 생성
-         mList.add(TodoData("Kotlin 공부하기", 4.5))
-         mList.add(TodoData("밥 먹기", 3.5))
-         mList.add(TodoData("강아지 산책시키기", 4.0))
-         mList.add(TodoData("장보러 다녀오기", 3.2))
-         mList.add(TodoData("심부름하기", 2.5))
-         mList.add(TodoData("내방 청소하기", 1.5))
+         mList.add(TodoData("Kotlin 공부하기", 4.5, "학원"))
+         mList.add(TodoData("밥 먹기", 3.5, "집"))
+         mList.add(TodoData("강아지 산책시키기", 4.0, "공원"))
+         mList.add(TodoData("장보러 다녀오기", 3.2, "시장"))
+         mList.add(TodoData("심부름하기", 2.5, "집"))
+         mList.add(TodoData("내방 청소하기", 1.5, "내방"))
 
         mListAdapter = TodoListViewAdapter(requireContext(), R.layout.todo_list_item, mList)
         todoListView.adapter = mListAdapter
 
         todoListView.setOnItemClickListener { adapterView, view, i, l ->
-            Toast.makeText(requireContext(), "${i}번째 아이템 선택됨", Toast.LENGTH_SHORT).show()
+            val myIntent = Intent(requireContext(), DetailTodoActivity::class.java)
+            myIntent.putExtra("todoData", mList[i])
+            startActivity(myIntent)
         }
     }
 
