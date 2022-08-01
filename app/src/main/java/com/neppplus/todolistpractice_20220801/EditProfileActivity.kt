e3cbbc12664fbf8723c919ed90f7c8fc0a709241
넵플러스 <nepp.plus@gmail.com>
2022-08-01 16:09:41 +0900

    변경값 작성후 돌아가는 인텐트 작업

--- app/src/main/java/com/neppplus/todolistpractice_20220801/EditProfileActivity.kt
+++ app/src/main/java/com/neppplus/todolistpractice_20220801/EditProfileActivity.kt
@@ -1,11 +1,26 @@
 package com.neppplus.todolistpractice_20220801
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_profile.*
 
 class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+
+        saveBtn.setOnClickListener {
+            val nick = nickEdt.text.toString()
+            val phoneNum = phoneNumEdt.text.toString()
+            val address = addressEdt.text.toString()
+
+            val resultIntent = Intent()
+            resultIntent.putExtra("nick", nick)
+                .putExtra("phoneNum", phoneNum)
+                .putExtra("address", address)
+            setResult(RESULT_OK, resultIntent)
+            finish()
+        }
     }
 }
