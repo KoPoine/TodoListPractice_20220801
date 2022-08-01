dfcda6b9f4fc7445c4a95614fcccc407fe023193
넵플러스 <nepp.plus@gmail.com>
2022-08-01 15:32:42 +0900

    버튼 색상, text 색상 동적(programatically) 변경 event

--- app/src/main/java/com/neppplus/todolistpractice_20220801/DetailTodoActivity.kt
+++ app/src/main/java/com/neppplus/todolistpractice_20220801/DetailTodoActivity.kt
@@ -2,10 +2,22 @@ package com.neppplus.todolistpractice_20220801
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.neppplus.todolistpractice_20220801.datas.TodoData
+import kotlinx.android.synthetic.main.activity_detail_todo.*
 
 class DetailTodoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_todo)
+
+        val todoData = intent.getSerializableExtra("todoData") as TodoData
+
+        titleTxt.text = todoData.title
+        placeTxt.text = todoData.place
+        todoRatingBar.rating = todoData.rating.toFloat()
+
+        deleteBtn.backgroundTintList = ContextCompat.getColorStateList(this, R.color.red)
+        placeTxt.setTextColor(ContextCompat.getColor(this, R.color.black))
     }
 }
