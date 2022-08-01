d04b6d21508f8690c2ddd6d3853556cfd7c97f8b
넵플러스 <nepp.plus@gmail.com>
2022-08-01 15:11:54 +0900

    세부 할일 목록 보여주는 layout 작성

---
+++ app/src/main/java/com/neppplus/todolistpractice_20220801/DetailTodoActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.todolistpractice_20220801
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailTodoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_todo)
+    }
+}
