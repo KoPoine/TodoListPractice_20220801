cdf7c8f56a8caf30eab7f85f2d90fdd1a42d8fe6
넵플러스 <nepp.plus@gmail.com>
2022-08-01 16:00:55 +0900

    프로필 변경 가는 로직 작성중

--- app/src/main/java/com/neppplus/todolistpractice_20220801/fragments/SettingFragment.kt
+++ app/src/main/java/com/neppplus/todolistpractice_20220801/fragments/SettingFragment.kt
@@ -1,13 +1,17 @@
 package com.neppplus.todolistpractice_20220801.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.todolistpractice_20220801.R
+import kotlinx.android.synthetic.main.fragment_setting.*
 
 class SettingFragment : Fragment(){
+
+    val REQ_FOR_EDIT = 1004
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +19,15 @@ class SettingFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        editBtn.setOnClickListener {
+            val myIntent = Intent(this, )
+            startActivityForResult(myIntent, REQ_FOR_EDIT)
+        }
+
     }
 
 }
