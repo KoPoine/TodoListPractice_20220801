66f94a534c9be20157241944164b41b6134269f6
넵플러스 <nepp.plus@gmail.com>
2022-08-01 13:19:17 +0900

    실제 데이터 연결 완료

--- app/src/main/java/com/neppplus/todolistpractice_20220801/adapters/TodoListViewAdapter.kt
+++ app/src/main/java/com/neppplus/todolistpractice_20220801/adapters/TodoListViewAdapter.kt
@@ -6,7 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inspector.IntFlagMapping
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.neppplus.todolistpractice_20220801.R
 import com.neppplus.todolistpractice_20220801.datas.TodoData
+import com.willy.ratingbar.ScaleRatingBar
+import kotlinx.android.synthetic.main.todo_list_item.view.*
 
 class TodoListViewAdapter(
     val mContext : Context,
@@ -20,6 +24,14 @@ class TodoListViewAdapter(
             tempRow = LayoutInflater.from(mContext).inflate(resId, null)
         }
         val row = tempRow!!
+
+        val data = mList[position]
+
+        val titleTxt = row.findViewById<TextView>(R.id.titleTxt)
+        val ratingBar = row.findViewById<ScaleRatingBar>(R.id.todoRatingBar)
+
+        titleTxt.text = data.title
+        ratingBar.rating = data.rating.toFloat()
 
         return row
     }
