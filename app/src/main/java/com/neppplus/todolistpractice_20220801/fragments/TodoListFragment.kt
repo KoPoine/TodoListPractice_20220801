6ddb3ca2395cf3a4e415400966c14bad9aab7b96
넵플러스 <nepp.plus@gmail.com>
2022-08-01 12:28:58 +0900

    ViewPager에 활용될 Fragment 3개 생성 완료

---
+++ app/src/main/java/com/neppplus/todolistpractice_20220801/fragments/TodoListFragment.kt
@@ -0,0 +1,20 @@
+package com.neppplus.todolistpractice_20220801.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.todolistpractice_20220801.R
+
+class TodoListFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_todo_list, container, false)
+    }
+
+}
